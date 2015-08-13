192bc7aa787dd29d30fa184b85dccf92782fda0f
marian marinov <marian.mmarinov@gmail.com>
2015-08-13 16:24:02 +0300

    under construction initial look of the blog

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Blog's code written in Laravel 5.1
+
+Visit the original blog on <a href="http://www.mmarinov.com">mmarinov.com</a>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>PhP and Laravel blog</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div id='main-container'>
+        <h1>Marian Marinov</h1>
+        <h4>The blog is under construction.</h4>
+        <div>When the lazy monkeys in my head get to work, it shouldn't be long to set it up.</div>
+        <div id='contact_email'>
+            <p>Contact me: marian.mmarinov@gmail.com</p>
+        </div>
+    </div>
+</body>
+<footer>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</footer>
+</html>
+
+
